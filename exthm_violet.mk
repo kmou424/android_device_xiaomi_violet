b19ca76b8201590c336f6914ce6f5ede93566d48
kmou424 <kmou424@arkose.cn>
2020-10-16 18:50:47 +0800

    violet: exTHmUI OFFICIAL

--- exthm_violet.mk
+++ exthm_violet.mk
@@ -10,6 +10,10 @@ $(call inherit-product, device/xiaomi/violet/device.mk)
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/exthm/config/common_full_phone.mk)
 
+# Build exTHmUI
+EXTHM_COMPILERTYPE := OFFICIAL
+EXTHM_BUILDTYPE :=
+
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := exthm_violet
 PRODUCT_DEVICE := violet
